6dcb4768268e4728dcdcca9b5a85b3ceb5af2a79
AlenRoySolidarios <142957953+SolidariosAlenRoy@users.noreply.github.com>
2023-09-05 19:57:23 -0700

    Add files via upload

---
+++ Book.php
@@ -0,0 +1,58 @@
+<?php
+
+    class book{ //CLASS
+        private string $title;
+        private string $author;
+        private int $pages;
+
+        //Properties/Attributes
+        public function __construct($title, $author, $pages){
+            $this->title = $title;
+            $this->author = $author;
+            $this->pages = $pages;
+        }
+
+        //Method
+        public function setTitle(string $title){
+            $this->title = $title; 
+        }
+
+        public function getTitle(){
+            return $this->title;
+        }
+        
+        public function setAuthor(string $author){
+            $this->author = $author; 
+        }
+        
+        public function getAuthor(){
+            return $this->author;
+        }
+
+        public function setPages(int $pages){
+            $this->pages = $pages; 
+        }
+
+        public function getPages(){
+            return $this->pages;
+        }
+
+        public function describe() :string {
+            return $this->title . ' by ' . $this->author . ' has ' . $this->pages . ' pages. '; 
+        }
+       
+    }
+
+    $book = new book("Harry Potter", "J.K. Rowling", 400);
+    echo $book->describe() . "\n";
+
+    $book->setTitle("New Title");
+    $book->setpages("400");
+    echo $book->describe();
+
+
+
+
+
+
+?>
